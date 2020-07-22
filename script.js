5b6debe4842b24e6d251a145df6f5bf62ee17e44
Steven Schaab <stevenes83@gmail.com>
2020-07-21 23:52:33 -0500

    password generator. 2nd push. almost working correctly

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate", writePassword);
-
-// Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
+var character = "";
+var skip = 0;
+var prePass = "";
+var numbChar = "0123456789".split("");
+var uppChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowChar = "abcdefghijklmnopqrstuvwxyz".split("");
+var symbChar = "!@#$%^&*(){}[]=<>/,.|~?".split("");
+//gives the button a function when clicked
+generateBtn.addEventListener("click", writePassword);
+//write function of buttion click
 function writePassword() {
-    enter = prompt("How many characters would you like your password? Choose between 8 and 128");
-    
+//first variable prompts a question for length
+    var enter = prompt("How many characters would you like your password? Choose between 8 and 128");
+//conditional checks to make sure the previous prompt is filled out and then goes on to confirm which characters will be used in the generator
     if (!enter) {
         alert("This needs a value");
         rewind = writePassword();
@@ -14,100 +24,55 @@ function writePassword() {
         rewind = writePassword();
     } 
     else {
-        confirmNumber = confirm("Will this contain numbers?");
-        confirmUppercase = confirm("Will this contain Uppercase letters?");
-        confirmLowercase = confirm("Will this contain Lowercase letters?");
-        confirmSymbol = confirm("Will this contain special characters?");
-    };
-
-    console.log(confirmNumber);
-    console.log(confirmUppercase);
-    console.log(confirmLowercase);
-    console.log(confirmSymbol);
-    console.log(enter);
-    console.log(writePassword);
-
-
+        var confirmNumber = confirm("Will this contain numbers?");
+        var confirmUppercase = confirm("Will this contain Uppercase letters?");
+        var confirmLowercase = confirm("Will this contain Lowercase letters?");
+        var confirmSymbol = confirm("Will this contain special characters?");
+    }
+//verifies that at least one character was chosen
     if (!confirmSymbol && !confirmNumber && !confirmUppercase && !confirmLowercase) {
-        choices = alert("You must choose a criteria!");
+        enter = alert("You must choose a criteria!");
         rewind = writePassword();
-    };
-  
-    if (confirmNumber){
-       function getRandomNumber(){
-            return String.fromCharCode[Math.floor(Math.random()*10)];
-            }
-    };
-    if (confirmUppercase){
-        function getRandomUpperCase(){
-            return String.fromCharCode(Math.floor(Math.random()*26));
-            }
-    };
-    if (confirmLowercase){
-        function getRandomLowerCase(){
-            return String.fromCharCode(Math.floor(Math.random()*26));
-            }
-    };
-    if (confirmSymbol){
-        function getRandomSymbol(){
-            var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-            return symbol[Math.floor(Math.random()*symbol.length)];
-            }
-    };
+    }
+  //says that if character wasnt selected to move on to the next when generating a password
+    if (!confirmNumber) {
+        skip++; 
+    }
+    if (!confirmUppercase) {
+        skip++;
+    }    
+    if (!confirmLowercase) {
+        skip++;
+    }
+    if (!confirmSymbol) {
+        skip++;
+    }    
 
-    const randomFunc = {
-        number : getRandomNumber,
-        upper : getRandomUpperCase,
-        lower : getRandomLowerCase,
-        symbol : getRandomSymbol
-    };
+    // if (skip === 0) {
+    //     alert("You must choose criteria.");
+    //     return null;
+    // }
 
-    console.log(number.value)
-
- 
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-    function generatePassword(upper, lower, number, symbol, length){
-    let generatedPassword = "";
-
-    const typesCount = upper + lower + number + symbol;
-
-    // console.log(typesCount);
-
-    const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-
-    if(typesCount === 0) {
-        return '';
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
 
-    for(let i=0; i<length; i+=typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        });
+    //loop that gets a random integer from the arrays selected until it reaches its length
+    for (var i = 0; i < enter; i += skip) {        
+        if (confirmNumber) {
+            prePass += numbChar[getRandomInt(enter)]
+        }
+        if (confirmUppercase) {
+            prePass += uppChar[getRandomInt(enter)]
+        }        
+        if (confirmLowercase) {
+            prePass += lowChar[getRandomInt(enter)]
+        }
+        if (confirmSymbol) {
+            prePass += symbChar[getRandomInt(enter)]
+        }    
     }
 
-    const finalPassword = generatedPassword.slice(0, length);
-
-
-    return finalPassword;
-    }
+password.innerText = prePass;
 }
-generateBtn.addEventListener("click", function() {
-    writePassword();
-    document.querySelector("#password").placeholder
-    });
-
-generateBtn.addEventListener('click', () =>{
-    const length = +enter.value;
-    const hasUpper = confirmUppercase.true;
-    const hasLower = confirmLowercase.true;
-    const hasNumber = confirmNumber.true;
-    const hasSymbol = confirmSymbol.true;
-
-document.querySelector("#password") = generatePassword(hasUpper, hasLower, hasNumber, hasSymbol, length);
-});
 
